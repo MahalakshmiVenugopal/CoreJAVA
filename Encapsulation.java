d7f7b56d7c50abd1112d59b109061386375c9835
MahalakshmiVenugopal <43148179+MahalakshmiVenugopal@users.noreply.github.com>
2020-03-04 21:28:38 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,21 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		
+		Book bo = new Book();
+		
+		//Direct Access
+		//bo.page_no = 100;
+		//System.out.println(bo.page_no);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the page number");
+		bo.setData(sc.nextInt());
+		System.out.println("Page number is " + bo.getData());
+	}
+
+}
